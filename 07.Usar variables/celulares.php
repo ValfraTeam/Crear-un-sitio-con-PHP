48afe4af1f0e5a0bdb55b7547577282eb5633873
Nebaris <hola@nebaris.com>
2015-09-09 07:01:11 -0300

    agregado ejemplo 07.usar variables

---
+++ 07.Usar variables/celulares.php
@@ -0,0 +1,14 @@
+<?php
+    $tituloDePagina = "Celulares";
+    include('inc/cabecera.php');
+?>
+
+    <section id="contenido">
+        <div class="contenedor">
+            <div class="celda12 center">
+                <h1>Celulares</h1>
+            </div>
+        </div>
+    </section>
+
+<?php include('inc/piedepagina.php'); ?>
---
+++ 07.Usar variables/inc/cabecera.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $tituloDePagina;?></title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link href='http://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+<header>
+    <div class="contenedor">
+        <div class="celda6">
+            <h1>
+                <a href="index.php">Nebaris celulares</a>
+            </h1>
+        </div>
+        <div class="celda6">
+            <ul>
+                <li><a href="celulares.php">Celulares</a>
+                </li>
+                <li><a href="contacto.php">Contacto</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
+<section id="encabezado">
+    <div class="contenedor">
+    </div>
+</section>
