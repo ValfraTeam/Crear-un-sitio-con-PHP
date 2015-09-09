c6aeffefc737b84f2141306794e208a8c422be83
Nebaris <hola@nebaris.com>
2015-09-09 07:45:58 -0300

    agregado ejemplo 08.usar condicionales

---
+++ 08.Usar condicionales/contacto.php
@@ -0,0 +1,15 @@
+<?php
+    $tituloDePagina = "Contacto";
+    $seccion = "contacto";
+    include('inc/cabecera.php');
+?>
+
+<section id="contenido">
+    <div class="contenedor">
+        <div class="celda12 center">
+            <h1>Contacto</h1>
+        </div>
+    </div>
+</section>
+
+<?php include('inc/piedepagina.php'); ?>
