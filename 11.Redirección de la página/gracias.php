38526400e5ede467c7694fd5b36764b006c6240e
Nebaris <hola@nebaris.com>
2015-09-11 06:40:04 -0300

    agregado el ejemplo 11.Redirección de la página

---
+++ 11.Redirección de la página/gracias.php
@@ -0,0 +1,16 @@
+<?php
+$tituloDePagina = "Contacto";
+$seccion = "contacto";
+include('inc/cabecera.php');
+?>
+
+<section id="contenido">
+    <div class="contenedor">
+        <div class="celda12 center">
+            <h1>Gracias</h1>
+            <p>Te estaremos respondiendo a la brevedad.</p>
+        </div>
+    </div>
+</section>
+
+<?php include('inc/piedepagina.php'); ?>
---
+++ 11.Redirección de la página/procesar-contacto.php
@@ -0,0 +1,13 @@
+<?php
+
+    $nombre = $_POST["nombre"];
+    $email = $_POST["email"];
+    $mensaje = $_POST["mensaje"];
+
+    $datos_usuario = "Nombre: " . $nombre . "<br>";
+    $datos_usuario .= "Email: " . $email . "<br>";
+    $datos_usuario .= "Mensaje: " . $mensaje;
+
+    // TODO: enviar email
+
+    header("Location: gracias.php");
