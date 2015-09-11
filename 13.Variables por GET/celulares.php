efa5b506277938989100ed499468ff389e8f2485
Nebaris <hola@nebaris.com>
2015-09-11 07:36:53 -0300

    agregado el ejemplo 13.Variables por GET

---
+++ 13.Variables por GET/celulares.php
@@ -0,0 +1,15 @@
+<?php
+    $tituloDePagina = "Celulares";
+    $seccion = "celulares";
+    include('inc/cabecera.php');
+?>
+
+    <section id="contenido">
+        <div class="contenedor">
+            <div class="celda12 center">
+                <h1>Celulares</h1>
+            </div>
+        </div>
+    </section>
+
+<?php include('inc/piedepagina.php'); ?>
