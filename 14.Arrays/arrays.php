5479a227fbecd53302580e02c7bac31b8fa211d6
Nebaris <hola@nebaris.com>
2015-09-14 07:48:38 -0300

    finalizado el ejemplo 14.arrays

--- 14.Arrays/arrays.php
+++ 14.Arrays/arrays.php
@@ -16,3 +16,59 @@ echo "Hay " . count($colores) . " colores";
 foreach($colores as $color) {
 	echo $color . "<br>";
 }
+
+// agregamos de a 1 elemento por vez
+$colores = array();
+$colores[] = "rojo";
+$colores[] = "verde";
+$colores[] = "azul";
+
+// indicamos una clave para cada elemento
+$array = array();
+$array[50] = "rojo";
+$array[33] = "verde";
+$array[4] = "azul";
+
+// creamos un array asociativo
+$computadora = array();
+$computadora["ram"] = 8;
+$computadora["cores"] = 4;
+$computadora["rigido"] = 500;
+
+// o de forma más compacta...
+$computadora = array(
+	"ram" => 8,
+	"cores" => 4,
+	"rigido" => 500
+);
+
+// mostramos un elemento
+echo $computadora["cores"];
+
+// creamos un array multidimensional o anidado
+$computadoras = array();
+$computadoras["escritorio"] = array (
+	"ram" => 8,
+	"cores" => 4,
+	"rigido" => 500
+);
+$computadoras["notebook"] = array (
+	"ram" => 4,
+	"cores" => 2,
+	"rigido" => 128
+);
+
+// mostramos un elementos
+echo $computadoras["notebook"]["cores"];
+
+?>
+
+// iteramos todos los elementos
+<?php foreach($computadoras as $descripcion=>$computadora) { ?>
+	<h2><?php echo $descripcion; ?></h2>
+	<ul>
+		<?php foreach($computadora as $propiedad=>$valor) { ?>
+			<li><?php echo $propiedad . " = " . $valor; ?></li>
+		<?php } ?>
+	</ul> 
+<?php } ?>
